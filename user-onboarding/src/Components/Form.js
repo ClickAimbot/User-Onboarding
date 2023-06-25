329328d0cc72560aa62d18b2a64c9acff0ba6452
Austin Amaral <austin.amaral3@gmail.com>
2023-06-25 06:58:45 -0700

    saving form component

---
+++ user-onboarding/src/Components/Form.js
@@ -0,0 +1,56 @@
+import react from 'react';
+
+const Form = (props) => {
+    const { change, submit } = props;
+    const { username, email, password, tos } = props.values;
+
+    onChange = (e) => {
+        const { name, value, checked, type } = e.target;
+    }
+
+    onSubmit = (e) => {
+        e.preventDefault();
+        submit();
+    }
+
+    return (
+        <div>
+            <h1>My cool form!</h1>
+            <form onSubmit={onSubmit}>
+                <Label> Name:
+                    <input 
+                        type="text"
+                        name="username"
+                        value={username}
+                        onChange={onChange}
+                    />
+                </Label>
+                <Label> Email:
+                    <input 
+                        type="email"
+                        name="email"
+                        value={email}
+                        onChange={onChange} 
+                    />
+                </Label>
+                <Label> Password
+                    <input 
+                        type="password"
+                        name="password"
+                        value={password}
+                        onChange={onChange} 
+                    />
+                </Label>
+                <Label> Terms of Service:
+                    <input 
+                        type="checkbox"
+                        name="tos"
+                        checked={checked}
+                        onChange={onChange} 
+                    />
+                </Label>
+                <input type="submit" value="Create a Friend!" />
+            </form>
+        </div>
+    )
+}
