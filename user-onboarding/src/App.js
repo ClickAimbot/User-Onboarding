2abbd83c709dcb38eaadf9f6897c0069e4a0d639
Austin Amaral <austin.amaral3@gmail.com>
2023-06-27 12:10:41 -0700

    Finished the app

--- user-onboarding/src/App.js
+++ user-onboarding/src/App.js
@@ -1,28 +1,63 @@
-import logo from './logo.svg';
 import React, { useState } from 'react'
 import './App.css';
 import Form from './Components/Form'
+import schema from './Validation/formSchema';
+import * as yup from 'yup'
+import axios from 'axios'
 
-const intialFormValues = {
+const initialFormValues = {
   username: "",
   email: "",
   password: "",
-  checked: false
+  tos: false
+}
+
+const initialFormErrors = {
+  username: "",
+  email: "",
+  password: "",
+  tos: ""
 }
 
 function App() {
   const [formValues, setFormValues] = useState(initialFormValues);
+  const [formErrors, setFormErrors] = useState(initialFormErrors);
+  const [users, setUsers] = useState([]);
 
   const handleSubmit = () => {
-    //WIP
+    axios.post('https://reqres.in/api/users', formValues)
+    .then(res => {
+      setUsers([ res.data, ...users ])
+    })
+    .catch(err => console.error(err))
+    .finally(() => setFormValues(initialFormValues))
+  }
+
+  const validate = (name, value) => {
+    yup.reach(schema, name)
+      .validate(value)
+      .then(() => setFormErrors({ ...formErrors, [name]: '' }))
+      .catch(err => setFormErrors({ ...formErrors, [name]: err.errors[0] }))
   }
 
   const handleChange = (name, value) => {
+    validate(name, value)
     setFormValues({...formValues, [name]: value})
   }
   return (
     <div className="App">
-      <Form values={formValues} />
+      <Form 
+        values={formValues} 
+        change={handleChange} 
+        errors={formErrors} 
+        submit={handleSubmit}
+      />
+      {users.map(user => (
+        <div key={user.id}>
+          <p>{user.createdAt}</p>
+          <p>{user.email}</p>
+        </div>
+    ))}
     </div>
   );
 }
--- user-onboarding/src/Components/Form.js
+++ user-onboarding/src/Components/Form.js
@@ -1,8 +1,8 @@
-import react from 'react';
+import React from 'react';
 
 const Form = (props) => {
-    const { change, submit } = props;
-    const { username, email, password, checked } = props.values;
+    const { change, submit, errors } = props;
+    const { username, email, password, tos } = props.values;
 
     const onChange = (e) => {
         const { name, value, checked, type } = e.target;
@@ -18,41 +18,46 @@ const Form = (props) => {
     return (
         <div>
             <h1>My cool form!</h1>
+            <p>{errors.username}</p>
+            <p>{errors.email}</p>
+            <p>{errors.password}</p>
+            <p>{errors.tos}</p>
             <form onSubmit={onSubmit}>
-                <Label> Name:
+                <label> Name:
                     <input 
                         type="text"
                         name="username"
                         value={username}
                         onChange={onChange}
                     />
-                </Label>
-                <Label> Email:
+                </label>
+                <label> Email:
                     <input 
                         type="email"
                         name="email"
                         value={email}
                         onChange={onChange} 
                     />
-                </Label>
-                <Label> Password
+                </label>
+                <label> Password
                     <input 
                         type="password"
                         name="password"
                         value={password}
                         onChange={onChange} 
                     />
-                </Label>
-                <Label> Terms of Service:
+                </label>
+                <label> Terms of Service:
                     <input 
                         type="checkbox"
                         name="tos"
-                        checked={checked}
+                        checked={tos}
                         onChange={onChange} 
                     />
-                </Label>
+                </label>
                 <input type="submit" value="Create a Friend!" />
             </form>
         </div>
     )
 }
+export default Form;
